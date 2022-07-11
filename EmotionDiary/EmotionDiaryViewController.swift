a70849199152b9459171b225731af285721f6152
dotori-0 <sesan131@gmail.com>
2022-07-11 23:39:59 +0900

    [Feat] Add Labels

--- EmotionDiary/EmotionDiaryViewController.swift
+++ EmotionDiary/EmotionDiaryViewController.swift
@@ -43,6 +43,11 @@ class EmotionDiaryViewController: UIViewController {
     var emotion7Count = 0
     var emotion8Count = 0
 
+//    var labelDict: [UILabel: String] = [:]
+    var labelArray: [UILabel] = []
+    var emotionCountArray: [Int] = []
+    var emotionDescription = ["행복해", "좋아해", "사랑해", "분노해", "심심해", "답답해", "당황해", "우울해", "한심해"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,11 +55,37 @@ class EmotionDiaryViewController: UIViewController {
         let buttonArray = [slime0Button, slime1Button, slime2Button, slime3Button, slime4Button,
                            slime5Button, slime6Button, slime7Button, slime8Button]
 
-        let labelArray = [emotion0Label, emotion1Label, emotion2Label, emotion3Label, emotion4Label,
-                          emotion5Label, emotion6Label, emotion7Label, emotion8Label]
+//        labelDict = [emotion0Label: "행복해",
+//                         emotion1Label: "좋아해",
+//                         emotion2Label: "사랑해",
+//                         emotion3Label: "분노해",
+//                         emotion4Label: "심심해",
+//                         emotion5Label: "답답해",
+//                         emotion6Label: "당황해",
+//                         emotion7Label: "우울해",
+//                         emotion8Label: "한심해"]
 
-//        slime0Button.inset
+//        let labelDict = [emotion0Label: ["행복해", 0],
+//                         emotion1Label: ["좋아해", 0],
+//                         emotion2Label: ["사랑해", 0],
+//                         emotion3Label: ["분노해", 0],
+//                         emotion4Label: ["심심해", 0],
+//                         emotion5Label: ["답답해", 0],
+//                         emotion6Label: ["당황해", 0],
+//                         emotion7Label: ["우울해", 0],
+//                         emotion8Label: ["한심해", 0]]
+
+        labelArray = [emotion0Label, emotion1Label, emotion2Label, emotion3Label, emotion4Label,
+                      emotion5Label, emotion6Label, emotion7Label, emotion8Label]
+
+        emotionCountArray = [emotion0Count, emotion1Count, emotion2Count, emotion3Count, emotion4Count,
+                                 emotion5Count, emotion6Count, emotion7Count, emotion8Count]
+
+
         designButtons(buttonArray)
+//        designLabels(labelDict: labelDict, emotionCountArray: emotionCountArray)
+        designLabels()
+
     }
 
     func designButtons(_ buttonArray: [UIButton?]) {
@@ -79,6 +110,45 @@ class EmotionDiaryViewController: UIViewController {
     }
 
 
+    // MARK: - Label이 Dictionary일 때
+//    func designLabels(labelDict: [UILabel?: [Any]], emotionCountArray: [Int]) {
+//    func designLabels(labelDict: [UILabel?: [Any]]) {
+//        for (key, value) in labelDict {
+////            labelArray[i][0]?.text = labelArray[i][1] + " " + String(emotionCountArray[i])
+//            key.text = value[0] + " " + String(value[1])
+//        }
+//    }
+
+    // O
+//    func designLabels(labelDict: [UILabel?: String], emotionCountArray: [Int]) {
+//        var i = 0
+//
+//        for (key, value) in labelDict {
+//            key?.backgroundColor = .clear
+//            key?.textAlignment = .center
+//            key?.text = value + " " + String(emotionCountArray[i])
+//            i += 1
+//        }
+//    }
+
+    //    func updateCount(selectedLabel: UILabel) {
+    //    func updateCount(selected: Int) {
+    //        labelDict[emotion\(selected)label]
+    //
+    //    }
+
+    // MARK: - Label이 Array일 때
+    func designLabels() {
+        for i in 0..<labelArray.count {
+            labelArray[i].backgroundColor = .clear
+            labelArray[i].textAlignment = .center
+            labelArray[i].text = emotionDescription[i] + " " + String(emotionCountArray[i])
+        }
+    }
+
+
+
+    // MARK: - Actions
 
 
 }
